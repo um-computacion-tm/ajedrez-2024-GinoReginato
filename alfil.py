b91dd75e609d43c6c0cde1abaea63a578130ad70
Gino Reginato <gp.reginato@alumno.um.edu.ar>
2024-08-25 22:14:20 -0300

    funcionamiento alfil

--- alfil.py
+++ alfil.py
@@ -2,6 +2,28 @@ from piece import Piece
 
 class Alfil(Piece):
 
-    def ValidMoves(self, actual_position):
+    def ValidMoves(self, actual_position,Board):
         
         x, y = actual_position
+
+        movimientos = []
+
+        # Movimientos diagonales
+        direcciones = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+        for dx, dy in direcciones:
+            nx, ny = x + dx, y + dy
+
+            while 0 <= nx < 8 and 0 <= ny < 8:
+                if board[nx][ny] is None:  # Celda vacía
+                    movimientos.append((nx, ny))
+                elif board[nx][ny].color != self.color:  # Pieza enemiga
+                    movimientos.append((nx, ny))
+                    break 
+                else:
+                    break 
+
+                nx += dx
+                ny += dy
+
+        return movimientos
