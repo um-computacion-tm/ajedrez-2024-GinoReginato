c57b5ee3c72fcb6cff42fa7e2778e26c8b00ee46
Gino Reginato <gp.reginato@alumno.um.edu.ar>
2024-08-28 19:00:19 -0300

    movimientos rey

--- king.py
+++ king.py
@@ -2,6 +2,25 @@ from piece import Piece
 
 class King(Piece):
 
-    def ValidMoves(self, actual_position):
+    def ValidMoves(self, actual_position, board):
         
         x, y = actual_position
+        movimientos = []
+
+       
+        direcciones = [
+            (1, 0), (-1, 0), (0, 1), (0, -1), 
+            (1, 1), (1, -1), (-1, 1), (-1, -1) 
+        ]
+
+        for dx, dy in direcciones:
+            nx, ny = x + dx, y + dy
+
+          
+            if 0 <= nx < 8 and 0 <= ny < 8:
+                if board[nx][ny] is None: 
+                    movimientos.append((nx, ny))
+                elif board[nx][ny].color != self.color: 
+                    movimientos.append((nx, ny))
+
+        return movimientos
