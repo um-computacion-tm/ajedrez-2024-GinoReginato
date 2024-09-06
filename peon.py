2b7253945d2f237c21b5a4d748077b583d3db514
Gino Reginato <gp.reginato@alumno.um.edu.ar>
2024-09-05 22:00:16 -0300

    implemnete un test para los mivimientos enemigos de la piezapeon

--- peon.py
+++ peon.py
@@ -1,6 +1,7 @@
 from piece import Piece
 
 class Peon(Piece):
+
     def __init__(self, color):
         super().__init__(color)
     
@@ -16,25 +17,21 @@ class Peon(Piece):
             if y == 1:  # Primer movimiento (se dan 2 pasos)
                 movimientos.append((x, y + 2))
 
-            # Manera de comer una pieza (Diagonal)
             if x > 0 and board[x - 1][y + 1] and board[x - 1][y + 1].__color__ == 'black':
                 capturas.append((x - 1, y + 1))
             if x < 7 and board[x + 1][y + 1] and board[x + 1][y + 1].__color__ == 'black':
                 capturas.append((x + 1, y + 1))
 
-        else:  # color negro
+        else: 
             movimientos.append((x, y - 1))
 
             if y == 6:
                 movimientos.append((x, y - 2))
 
-            # Manera de comer una pieza (Diagonal)
-            if x > 0 and board[x - 1][y + 1] and board[x - 1][y + 1].__color__ == 'white':
-                capturas.append((x - 1, y + 1))
-
         
-            if x < 7 and board[x + 1][y + 1] and board[x + 1][y + 1].__color__ == 'white':
-                capturas.append((x + 1, y + 1))
+            if x > 0 and board[x - 1][y - 1] and board[x - 1][y - 1].__color__ == 'white':
+                capturas.append((x - 1, y - 1))
+            if x < 7 and board[x + 1][y - 1] and board[x + 1][y - 1].__color__ == 'white':
+                capturas.append((x + 1, y - 1))
 
-        return movimientos + capturas
-
+        return movimientos + capturas 
--- test_peon.py
+++ test_peon.py
@@ -1,22 +1,43 @@
 import unittest
+from piece import Piece
 from peon import Peon
-from board import Board
 
 class TestPeon(unittest.TestCase):
     
-    def setUp(self):
-        self.board = Board()
-
-
-    def test_white_pawn_initial_moves(self):
+    def test_movimientos_peon_blanco(self):
         peon_blanco = Peon('white')
-        self.assertEqual(peon_blanco.ValidMoves((3, 1), self.board.position), [(3, 2), (3, 3)])  # Movimientos de peón blanco
+        board = [[None for _ in range(8)] for _ in range(8)] 
+        
+        movimientos = peon_blanco.ValidMoves((1, 1), board)
+        self.assertIn((1, 2), movimientos)
+        self.assertIn((1, 3), movimientos) 
 
-    def test_black_pawn_initial_moves(self):
+    def test_movimientos_peon_negro(self):
         peon_negro = Peon('black')
-        self.assertEqual(peon_negro.ValidMoves((4, 6), self.board.position), [(4, 5), (4, 4)])  # Movimientos de peón negro
+        board = [[None for _ in range(8)] for _ in range(8)] 
+        
+        movimientos = peon_negro.ValidMoves((6, 6), board)
+        self.assertIn((6, 5), movimientos) 
+        self.assertIn((6, 4), movimientos) 
     
-    
+    def test_movimientos_con_pieza_blanca(self):
+        peon_blanco = Peon('white')
+        board = [[None for _ in range(8)] for _ in range(8)] 
+        peon_negro = Peon('black')
+        board[2][2] = peon_negro  
+        
+        movimientos = peon_blanco.ValidMoves((1, 1), board)
+        self.assertIn((2, 2), movimientos) 
+
+    def test_movimientos_con_pieza_negra(self):
+        peon_negro = Peon('black')
+        board = [[None for _ in range(8)] for _ in range(8)] 
+        peon_blanco = Peon('white')
+        board[5][5] = peon_blanco 
+        
+        movimientos = peon_negro.ValidMoves((6, 6), board)
+        self.assertIn((5, 5), movimientos)
+
 if __name__ == '__main__':
     unittest.main()
 
