c96eb08798e171e538c0bd3477760a0f468dda3e
Gino Reginato <gp.reginato@alumno.um.edu.ar>
2024-09-06 11:43:17 -0300

    nueva excepcion(outofboarderror)

--- cli.py
+++ cli.py
@@ -15,7 +15,10 @@ def play(chess):
         to_row = int(input("To Row: "))
         to_col = int(input("To Col: "))
 
-        # :)
+        if not (0 <= from_row < 8 and 0 <= from_col < 8 and 0 <= to_row < 8 and 0 <= to_col < 8):
+            raise OutOfBoardError("Movimiento fuera de tablero fuera del tablero")
+
+           # :)
         chess.move(
 
             from_row,
@@ -24,11 +27,14 @@ def play(chess):
             to_col,
         )
 
+    except OutOfBoardError as e:
+        print(e)
     except InvalidMove as e:
-        print("Movimiento no valido")
+        print("Movimiento no válido")
     except Exception as e:
-        print("error", e)
+        print("Error:", e)
 
 
 if __name__ == '__main__':
     main()
+
