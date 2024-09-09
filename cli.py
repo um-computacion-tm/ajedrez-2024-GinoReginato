9ef8d9283b0e3f4105ab1bb741b458d0cda927bf
Gino Reginato <gp.reginato@alumno.um.edu.ar>
2024-09-09 15:41:56 -0300

    arregle test cli

--- cli.py
+++ cli.py
@@ -7,14 +7,15 @@ def main():
 
 def play(chess):
     try:
-        # print(chess.show_board())
         print("turn: ", chess.turn)
         from_row = int(input("From row: "))
         from_col = int(input("From col: "))
         to_row = int(input("To Row: "))
         to_col = int(input("To Col: "))
 
-        if not (0 <= from_row < 8 and 0 <= from_col < 8 and 0 <= to_row < 8 and 0 <= to_col < 8):
+        
+        
+        if not (0 <= from_row <= 7 and 0 <= from_col <= 7 and 0 <= to_row <= 7 and 0 <= to_col <= 7):
             raise OutOfBoardError("Movimiento fuera del tablero")
 
         chess.move(from_row, from_col, to_row, to_col)
--- test_cli.py
+++ test_cli.py
@@ -1,28 +1,43 @@
 import unittest
 from unittest.mock import patch
 from chess import Chess
-from cli import play
-from cli import OutOfBoardError, InvalidMove
+from cli import *
+
 
 class TestCli(unittest.TestCase):
 
-    @patch("builtins.input", side_effect=['1', '1', '2', '2'])
-    @patch("builtins.print")
-    def test_valid_move(self, mock_print, mock_input):
+    @patch('builtins.input', side_effect=[1, 1, 2, 2])
+    @patch('builtins.print')
+    @patch.object(Chess, 'move')
+    def test_valid_move(self, 
+    mock_chess_move, 
+    mock_print, 
+    mock_input
+    ):
         chess = Chess()
         play(chess)
     
-        mock_print.assert_any_call("turn: ", chess.turn)
+        self.assertEqual(mock_input.call_count, 4)
+        self.assertEqual(mock_print.call_count, 1)
+        self.assertEqual(mock_chess_move.call_count, 1)
 
-    @patch("builtins.input", side_effect=['-1', '0', '2', '2'])
+    @patch("builtins.input", side_effect=[-1, 0, 2, 2])
     @patch("builtins.print")
-    def test_out_of_board_move(self, mock_print, mock_input):
+    @patch.object(Chess, 'move')
+    def test_out_of_board_move(
+        self, 
+        mock_chess_move, 
+        mock_print, 
+        mock_input
+        ):
         chess = Chess()
-        with self.assertRaises(OutOfBoardError):
-            play(chess)
-        mock_print.assert_called_with("Movimiento fuera de tablero")
+        play(chess)
+        self.assertEqual(mock_input.call_count, 4)
+        self.assertEqual(mock_print.call_count, 2)
+        self.assertEqual(mock_chess_move.call_count, 0)
 
-    @patch("builtins.input", side_effect=['1', '1', '2', '2'])
+
+    @patch("builtins.input", side_effect=[1, 1, 2, 2])
     @patch("builtins.print")
     def test_invalid_move(self, mock_print, mock_input):
         chess = Chess()
